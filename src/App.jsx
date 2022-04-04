7941990d5413463a2719d2bae5356fc763eff2e8
Abentancor <Angelbentancor@gmail.com>
2022-04-04 18:16:26 -0300

    Lista de tareas

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todolist from './Componentes/Todolist';
+
+const App = () => {
+  return <div className='container'>
+      <h1>App</h1>
+     <Todolist/>
+  </div>
+}
+export default App;
